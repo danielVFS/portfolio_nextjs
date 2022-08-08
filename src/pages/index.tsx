44a72657e8e7011516c7c3cbbc71845c47d577b5
danielVFS <danielv759153@gmail.com>
2022-08-08 15:04:18 -0300

    feat(home): basic introduction

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,23 @@
+import { Box, Container, Heading } from "@chakra-ui/react";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <p>hello world</p>;
+  return (
+    <Container>
+      <Box borderRadius="lg" bg="red" p={3} mb={6} textAlign="center">
+        Olá, Sou o Daniel, um desenvolvedor frontend que mora no Brasil!
+      </Box>
+
+      <Box display={{ md: "flex" }}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            Daniel Vitor
+          </Heading>
+          <p>Desenvolvedor Frontend (React.js/Angular)</p>
+        </Box>
+      </Box>
+    </Container>
+  );
 };
 
 export default Home;
