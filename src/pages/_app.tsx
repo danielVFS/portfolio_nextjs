42100df931858610207b2fc49a46734b7266d893
danielVFS <danielv759153@gmail.com>
2022-08-09 10:51:30 -0300

    feat(Fonts): create component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import { Fonts } from "../components/Fonts";
 import { Main } from "../components/layouts/Main";
 import theme from "../lib/theme";
 
 function MyApp({ Component, pageProps, router }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <Fonts />
       <Main router={router}>
         <Component {...pageProps} key={router.route} />
       </Main>
