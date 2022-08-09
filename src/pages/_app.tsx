445cccdb52ae39a70e9ef37b8349f0ee18e7fdda
danielVFS <danielv759153@gmail.com>
2022-08-08 21:47:39 -0300

    feat(theme): add theme on chakra

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import { Main } from "../components/layouts/Main";
+import theme from "../lib/theme";
 
 function MyApp({ Component, pageProps, router }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Main router={router}>
         <Component {...pageProps} key={router.route} />
       </Main>
