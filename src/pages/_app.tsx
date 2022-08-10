b78e80c81f1ae8cbe3be5c22708b34343dfeeccd
danielVFS <danielv759153@gmail.com>
2022-08-10 20:59:05 -0300

    feat(Layout): add animation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import { AnimatePresence } from "framer-motion";
 import type { AppProps } from "next/app";
 import { Fonts } from "../components/Fonts";
 import { Main } from "../components/layouts/Main";
@@ -9,7 +10,17 @@ function MyApp({ Component, pageProps, router }: AppProps) {
     <ChakraProvider theme={theme}>
       <Fonts />
       <Main router={router}>
-        <Component {...pageProps} key={router.route} />
+        <AnimatePresence
+          exitBeforeEnter
+          initial={true}
+          onExitComplete={() => {
+            if (typeof window !== "undefined") {
+              window.scrollTo({ top: 0 });
+            }
+          }}
+        >
+          <Component {...pageProps} key={router.route} />
+        </AnimatePresence>
       </Main>
     </ChakraProvider>
   );
--- src/pages/works.tsx
+++ src/pages/works.tsx
@@ -1,24 +1,16 @@
 import { Container, Heading } from "@chakra-ui/react";
 import type { NextPage } from "next";
-import dynamic from "next/dynamic";
 import { Layout } from "../components/layouts/Layout";
 import { Section } from "../components/Section";
 import { SkillsAndExperience } from "../components/SkillsAndExperience";
 
-const Bio = dynamic(() => import("../components/Bio"), {
-  ssr: false,
-});
-
-const Home: NextPage = () => {
+const Works: NextPage = () => {
   return (
-    <Layout title="Home">
+    <Layout title="Trabalhos">
       <Container maxW="1200px" display="flex" flexDir="column" gap={6} mt={6}>
-        <Bio />
-      </Container>
-      <Container maxW="1200px" display="flex" flexDir="column" gap={6} mt={6}>
-        <Section id="skills" delay="0.1">
+        <Section id="works" delay="0.1">
           <Heading as="h3" variant="section-title">
-            Habilidades e Experiências
+            Trabalhos
           </Heading>
           <SkillsAndExperience />
         </Section>
@@ -27,4 +19,4 @@ const Home: NextPage = () => {
   );
 };
 
-export default Home;
+export default Works;
