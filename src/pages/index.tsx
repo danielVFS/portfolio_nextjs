a518dc94f802c551c95adebf67b34233ca879968
danielVFS <danielv759153@gmail.com>
2022-08-10 01:25:42 -0300

    feat: new assets

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import type { NextPage } from "next";
 import dynamic from "next/dynamic";
 import { Profile } from "../components/Profile";
 import { Section } from "../components/Section";
+import { Skills } from "../components/Skills";
 
 const DynamicBio = dynamic(() => import("../components/Bio"), {
   ssr: false,
@@ -10,18 +11,21 @@ const DynamicBio = dynamic(() => import("../components/Bio"), {
 
 const Home: NextPage = () => {
   return (
-    <Container maxW="800px" display="flex" flexDir="column" gap={6} mt={6}>
-      <DynamicBio />
+    <>
+      <Container maxW="800px" display="flex" flexDir="column" gap={6} mt={6}>
+        <DynamicBio />
 
-      <Profile />
-
-      <Section delay="0.1">
-        <Heading as="h3" variant="section-title">
-          Habilidades
-        </Heading>
-        <p>hello</p>
-      </Section>
-    </Container>
+        <Profile />
+      </Container>
+      <Container maxW="1200px" display="flex" flexDir="column" gap={6} mt={6}>
+        <Section delay="0.1">
+          <Heading as="h3" variant="section-title">
+            Habilidades e Experiências
+          </Heading>
+          <Skills />
+        </Section>
+      </Container>
+    </>
   );
 };
 
