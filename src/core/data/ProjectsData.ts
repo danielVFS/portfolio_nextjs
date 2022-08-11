f4e1773ab920de3a37e532cacf10659239809fd3
danielVFS <danielv759153@gmail.com>
2022-08-11 16:34:41 -0300

    refactor: create projects data

---
+++ src/core/data/ProjectsData.ts
@@ -0,0 +1,23 @@
+export const projects = [
+  {
+    image: "world-trip-demo",
+    project_name: "World Trip",
+    stack: [
+      { tech: "React.js", image: "react" },
+      { tech: "Next.js", image: "next-js" },
+      { tech: "Chakra UI", image: "chakra" },
+      { tech: "Figma", image: "figma" },
+    ],
+  },
+  {
+    image: "next-blog-demo",
+    project_name: "Next Blog",
+    stack: [
+      { tech: "React.js", image: "react" },
+      { tech: "Next.js", image: "next-js" },
+      { tech: "Styled Components", image: "styled-components" },
+      { tech: "Storybook", image: "storybook" },
+      { tech: "Jest", image: "jest" },
+    ],
+  },
+];
