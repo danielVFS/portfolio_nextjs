ea296a33d94741af749e015531a37364fa259bcc
danielVFS <danielv759153@gmail.com>
2022-08-17 16:33:08 -0300

    refactor(timeline): small date to bigger

--- src/components/home/Timeline.tsx
+++ src/components/home/Timeline.tsx
@@ -8,14 +8,14 @@ interface TimelineProps {
 export function Timeline({ initialDate, finalDate }: TimelineProps) {
   return (
     <Flex flexDir="column" align="center">
-      <Text mb={3}>{initialDate}</Text>
+      <Text mb={3}>{finalDate}</Text>
       <Box
         width="3px"
         height="80px"
         borderRadius="full"
         bg={useColorModeValue("whiteAlpha.800", "whiteAlpha.200")}
       />
-      <Text mt={3}>{finalDate}</Text>
+      <Text mt={3}>{initialDate}</Text>
     </Flex>
   );
 }
